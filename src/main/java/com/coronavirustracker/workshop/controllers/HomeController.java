9b1e569281f40dfbc376785d6da9ddec54b14373
Hassan <hassanalihazaraa@gmail.com>
2020-10-21 15:41:03 +0200

    sum of all confirmed cases

--- src/main/java/com/coronavirustracker/workshop/controllers/HomeController.java
+++ src/main/java/com/coronavirustracker/workshop/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.coronavirustracker.workshop.controllers;
 
+import com.coronavirustracker.workshop.models.LocationStats;
 import com.coronavirustracker.workshop.services.CoronaVirusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +17,11 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("locationStats", coronaVirusService.getAllStats());
+        List<LocationStats> allStats = coronaVirusService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+
         return "index";
     }
 }
