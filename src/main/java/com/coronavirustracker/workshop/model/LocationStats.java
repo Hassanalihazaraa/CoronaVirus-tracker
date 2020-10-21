69bf4467371e3dec352866085fe8e77bf5cd8e09
Hassan <hassanalihazaraa@gmail.com>
2020-10-21 14:28:11 +0200

    parse csv data to string and only show the country and province

---
+++ src/main/java/com/coronavirustracker/workshop/model/LocationStats.java
@@ -0,0 +1,5 @@
+package com.coronavirustracker.workshop.model;
+
+public class LocationStats {
+
+}
--- src/main/java/com/coronavirustracker/workshop/services/CoronaVirusService.java
+++ src/main/java/com/coronavirustracker/workshop/services/CoronaVirusService.java
@@ -1,23 +1,35 @@
 package com.coronavirustracker.workshop.services;
 
-import ch.qos.logback.core.net.server.Client;
-import org.springframework.boot.autoconfigure.integration.IntegrationProperties;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@Service
 public class CoronaVirusService {
     private static String CORONA_VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
+    @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchUrlData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(CORONA_VIRUS_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
     }
 }
