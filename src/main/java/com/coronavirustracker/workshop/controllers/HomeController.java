46af1b158e61389b5d728e1b3d8888416771ce48
Hassan <hassanalihazaraa@gmail.com>
2020-10-21 15:24:24 +0200

    created a tabl for better data visualization

--- src/main/java/com/coronavirustracker/workshop/controllers/HomeController.java
+++ src/main/java/com/coronavirustracker/workshop/controllers/HomeController.java
@@ -14,7 +14,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("LocationStats", coronaVirusService.getAllStats());
+        model.addAttribute("locationStats", coronaVirusService.getAllStats());
         return "index";
     }
 }
