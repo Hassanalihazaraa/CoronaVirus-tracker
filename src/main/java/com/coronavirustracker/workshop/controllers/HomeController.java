ce5e623799c0c9f4f937df0b38a17d89b91a9b1c
Hassan <hassanalihazaraa@gmail.com>
2020-10-21 15:12:48 +0200

    get all locationStats from service and autowired it to controller and display on template

--- src/main/java/com/coronavirustracker/workshop/controllers/HomeController.java
+++ src/main/java/com/coronavirustracker/workshop/controllers/HomeController.java
@@ -1,4 +1,20 @@
 package com.coronavirustracker.workshop.controllers;
 
+import com.coronavirustracker.workshop.services.CoronaVirusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @Autowired
+    CoronaVirusService coronaVirusService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("LocationStats", coronaVirusService.getAllStats());
+        return "index";
+    }
 }
--- src/main/java/com/coronavirustracker/workshop/services/CoronaVirusService.java
+++ src/main/java/com/coronavirustracker/workshop/services/CoronaVirusService.java
@@ -18,9 +18,13 @@ import java.util.List;
 
 @Service
 public class CoronaVirusService {
-    private static String CORONA_VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private static String CORONA_VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     private List<LocationStats> allStats = new ArrayList<>();
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
 
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
